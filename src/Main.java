60ca2b475e7d8bf64fd67c134699d2aa72761c4a
Elena Zhikhareva <ursalet@gmail.com>
2024-07-18 15:20:28 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int replenishmentAmount = 1000;
+        int bonus;
+        int totalAmount;
+
+        if (replenishmentAmount < 1000) {
+            else bonus = 0;
+        }
+
+        if (replenishmentAmount > 1000) {
+            else bonus = replenishmentAmount / 100;
+
+        System.out.println(totalAmount = balance + replenishmentAmount + bonus);
+
+
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
